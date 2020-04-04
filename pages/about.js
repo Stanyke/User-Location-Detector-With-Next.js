66ae86bc41bad2cb2578b440a97f417adca87044
Stanley Chibuike <chibuike.stanley50@gmail.com>
2020-04-04 11:17:43 +0100

    Updated Index & About page

--- pages/about.js
+++ pages/about.js
@@ -8,7 +8,7 @@ const About = () =>
 
             <p>This web application was built on Next.js a React.js Framework, which retrieves user's current location and providing more details on it, including it's Internet Service Provider Details with the help of an external API <b>http://ip-api.com/json</b> which check's user exact location on page load.</p>
         </div>
-        
+
         <style jsx>
         {`
             p{
