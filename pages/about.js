cf444c15f8dc85bd2f8b086ab8749ef8a50a2f37
Stanley Chibuike <chibuike.stanley50@gmail.com>
2020-04-04 10:56:48 +0100

    Updated About page

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,9 @@ const About = () =>
 (
     <Layout>
         <div>
-            <h2>This is the about page</h2>
+            <center><h2>User-Location-Detector-With-Next.js</h2></center>
+            
+            <p>This web application was built on Next.js a React.js Framework, which retrieves user's current location and providing more details on it, including it's Internet Service Provider Details with the help of an external API <b>http://ip-api.com/json</b> which check's user exact location on page load.</p>
         </div>
     </Layout>
 )
